09aca88963186f40cb1faf3d7df41418a57584ea
Tolga Köyluoglu <tolga.koyluoglu1@gmail.com>
2022-09-23 16:49:23 +0200

    fix: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,28 @@
+/* eslint-disable prettier/prettier */
 module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
   extends: [
-    "airbnb",
-    "airbnb-typescript",
-    "airbnb/hooks",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   overrides: [],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: "**/tsconfig.json",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: '**/tsconfig.json',
   },
-  plugins: ["react", "@typescript-eslint", "prettier"],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
-    "react/react-in-jsx-scope": 0,
-    "import/prefer-default-export": 0,
+    'react/react-in-jsx-scope': 0,
+    'import/prefer-default-export': 0,
+    'no-console': 0,
   },
-};
+}
