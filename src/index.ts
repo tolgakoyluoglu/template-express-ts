40a1dfebd8eaad438b5780278d015b96e735fb06
Tolga Köyluoglu <tolga.koyluoglu1@gmail.com>
2022-09-23 16:44:32 +0200

    fix: test lint

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ if (process.env.PORT) {
   const PORT: number = parseInt(process.env.PORT, 10)
   const HOST: string = process.env.HOST ?? '0.0.0.0'
   app.listen(PORT, HOST, () => {
-    console.log('\x1b[36m%s\x1b[0m', `API URL: http://${HOST}:${PORT}`)
+     console.log('\x1b[36m%s\x1b[0m', `API URL: http://${HOST}:${PORT}`)
     console.log('\x1b[36m%s\x1b[0m', `Docs URL: http://${HOST}:${PORT}/docs`)
   })
 }
